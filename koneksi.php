0e5c5428e49b5caf71a54b99f49eef568e982c8c
Muhammad Salman Abid <42663086+beecoder77@users.noreply.github.com>
2018-10-26 11:24:51 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,17 +1,14 @@
 <?php
-class DB_Connect {
-    private $conn;
- 
-    // koneksi ke database
-    public function connect() {
-        require_once 'config.php';
-         
-        // koneksi ke mysql database
-        $this->conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
-         
-        // return database handler
-        return $this->conn;
-    }
+
+$db_host = "localhost";
+$db_user = "beecoder77_abid";
+$db_pass = "Semar@365%";
+$db_name = "beecoder77_abid";
+
+try {    
+    //create PDO connection 
+    $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+} catch(PDOException $e) {
+    //show error
+    die("Terjadi masalah: " . $e->getMessage());
 }
- 
-?>
